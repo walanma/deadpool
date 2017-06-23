dd9cbd1e28bc2c347b7aa79f37d937ebb46c427e
deadpooldata <deadpooldata@gmail.com>
2017-06-23 19:11:40 +1200

    updates

--- scripts/Data_cleaning.py
+++ scripts/Data_cleaning.py
@@ -5,41 +5,56 @@ Created on Wed Jun 21 20:57:48 2017
 @author: water
 """
 #grabbing pharmacy name from csv data -- usually first value line 2
+import glob
 import csv
-with open('test.csv') as csvfile:
-    readCSV = csv.reader(csvfile)
-    value = []
-    for line in readCSV:
-        value.append(line)
-    for v in value[1]:
-        if v != "":
-            pharmacy_name = v
-
-#Cleaning data
 import pandas as pd
 import numpy as np
-df = pd.read_csv('c:\Github\dp\internal\deadpool\data\merge_raw.csv', \
-#           skiprows=9, \
-           names=['Pharmacode', 'Product', 'Pack Size', 'SOH', 'W/S Price', 'SOH Value'],\
+path =r'C:\Github\dp\internal\deadpooldata'
+allFiles = glob.glob(path + "/*.csv")
+frame = pd.DataFrame()
+list_ = []
+#Cleaning data
+for file_ in allFiles:
+    with open(file_) as csvfile:
+        readCSV = csv.reader(csvfile)
+        value = []
+        for line in readCSV:
+            value.append(line)
+        for v in value[1]:
+            if v != "":
+                pharmacy_name = v
+            
+                print(pharmacy_name)
+    
+    df = pd.read_csv(file_, \
+                     skiprows=9, \
+                     names=['Pharmacode', 'Product', 'Locn','Pack Size','Manf', 'SO', 'SOH', 'Adj','W/S Price', 'SOH Value'],\
                 )
+#dropping unneccessary columns
+    del df['Locn']
+    del df['Manf']
+    del df['Adj']
+    del df['SO']
+
 #inserting a column
-df.insert(0, "Store Name", pharmacy_name)
+    df.insert(0, "Store Name", pharmacy_name)
 #rearrange columns
-df = df[['Store Name','Product', 'Pharmacode', 'Pack Size', 'SOH', 'W/S Price', 'SOH Value']]
+    df = df[['Store Name','Product', 'Pharmacode', 'Pack Size', 'SOH', 'W/S Price', 'SOH Value']]
 #dropping unneeded rows from multiple pages
-df2 = df[pd.notnull(df['SOH'])]
-df3 = df2[df.Pharmacode != 'PCode']
-df4 = df3[df.Product != 'Unnamed: 1']
-df4.to_csv('c:\Github\dp\internal\deadpool\data\DeadStockData.csv', index=False)
+    df2 = df[pd.notnull(df['SOH'])]
+    df3 = df2[df.Product != ' Product']
+    list_.append(df3)
+frame = pd.concat(list_)
+frame.to_csv('C:\Github\dp\internal\deadpool\data\DeadStockData.csv', index = False)
 
 #getting number of unique pharmacies
-unique_pharmacies = df4['Store Name'].nunique()
+unique_pharmacies = frame['Store Name'].nunique()
 #getting Total SOH Value for all pharmacies
 print(unique_pharmacies)
-Total_SOH_Value = df4['SOH Value'].astype(float)
+Total_SOH_Value = frame['SOH Value'].astype(float)
 TSH = Total_SOH_Value.sum()
 rounded_total = round(TSH,2)
-
+print(rounded_total)
 #Reading index.html and editing html to replace phrases
 import urllib.request
 page = urllib.request.urlopen("file:///Github/dp/internal/local_index.html")
