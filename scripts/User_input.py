1452c728171e4d8bd6769ca70f4016ef3ad32f6d
Deadpool Admin <walanma@gmail.com>
2017-08-04 20:28:35 +1200

    Daily merged update

--- scripts/User_input.py
+++ scripts/User_input.py
@@ -11,10 +11,8 @@ cols = ['Store Name','Product','Pharmacode','Pack Size','SOH', 'W/S Price', 'SOH
 d2 = df2.set_index(cols).Expiry.dropna()
 #print(d2)
 df3 = df1.fillna(df1.drop('Expiry', 1).join(d2, on=cols))
-d4 = df2.set_index(cols).Comments.dropna()
-df4 = df3.fillna(df3.drop('Comments', 1).join(d4, on=cols))
 #print(df4)
-df4.to_csv('C:\Github\dp\internal\deadpool\data\DeadStockData.csv', index=False)
-df4.to_csv('C:\Github\dp\internal\deadpool\data\Input.csv', index=False)
+df3.to_csv('C:\Github\dp\internal\deadpool\data\DeadStockData.csv', index=False)
+df3.to_csv('C:\Github\dp\internal\deadpool\data\Input.csv', index=False)
 
 #print(df3)
